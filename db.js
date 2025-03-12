7068070f9bc00ccc02e10d38b88c759bb79c728d
Suvam093 <suvamshaw86@gmail.com>
2025-03-12 11:49:25 +0530

    adding tls validation igonore

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 // const mongoURL = process.env.MONGODB_LOCAL_URL;
 const mongoURL = process.env.DB_URL;
 
-mongoose.connect(mongoURL)
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tlsAllowInvalidCertificates: true // Ignore TLS validation errors
+  })
 
 const db = mongoose.connection;
 
